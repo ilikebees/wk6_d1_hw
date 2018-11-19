0b343f23088ce1341f80a5b3cc7ae836fff92a03
Laura Manson <hellolauramanson@gmail.com>
2018-11-19 22:48:59 +0000

    MVP

---
+++ wk6_d1_hw.js
@@ -0,0 +1,244 @@
+// # Scope Homework: Who Dunnit
+//
+// ### Learning Objectives
+//
+// - Understand function scope
+// - Know the difference in between the let and const keywords
+//
+// ## Brief
+//
+// Using your knowledge about scope and variable declarations in JavaScript, look at the following code snippets and predict what the output or error will be and why. Copy the following episodes into a JavaScript file and add comments under each one detailing the reason for your predicted output.
+//
+// ### MVP
+//
+// #### Episode 1
+//
+// ```js
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+// ```
+//
+// Episode 1 Answer:
+// output: The murderer is Miss Scarlet.
+// reason:
+//
+// #### Episode 2
+//
+// ```js
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// ```
+// Episode 2 Answer:
+// output: Error message.
+// reason: You can't reassign a const. You can add to it but not reassign it.
+// We are getting an error message because we are trying to reassign a the const murderer? I'm not 100% clear on this one.
+
+// #### Episode 3
+//
+// ```js
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+// ```
+// Episode 3 Answer:
+// output: First Verdict: The murderer is Mrs Peacock.
+// Second Verdict: The murderer is Professor Plum.
+// Reason: let is block scoped. Meaning it doesn't exist outside the block it is declared in. Anything outside the block won't be able to access the let declared within that block.
+//
+// #### Episode 4
+//
+// ```js
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+// ```
+// Episode 4 Answer:
+// output: The suspects are Miss Scarlett, Professor Plum and Colonel Mustard.
+// Suspect three is Mrs Peacock.
+// Reason: Mrs Peacock is assigned with a let outside of the function, hence why "Suspect three is Mrs Peacock".
+//
+// #### Episode 5
+//
+// ```js
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+//
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+//
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+// ```
+// Episode 5 Answer:
+// Output: The weapon is the Revolver. I don't quite understand how this is the case.
+//
+// #### Episode 6
+//
+// ```js
+// let murderer = 'Colonel Mustard';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// ```
+// Episode 6 Answer:
+// Output:
+// Reason: I don't fully understand how this one works.
+//
+// #### Episode 7
+//
+// ```js
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+// ```
+// Episode 7 Answer:
+// Output:
+// Reason:
+
+// #### Episode 8
+
+// ```js
+// const scenario = {
+//   murderer: 'Mrs. Peacock',
+//   room: 'Conservatory',
+//   weapon: 'Lead Pipe'
+// };
+//
+// const changeScenario = function() {
+//   scenario.murderer = 'Mrs. Peacock';
+//   scenario.room = 'Dining Room';
+//
+//   const plotTwist = function(room) {
+//     if (scenario.room === room) {
+//       scenario.murderer = 'Colonel Mustard';
+//     }
+//
+//     const unexpectedOutcome = function(murderer) {
+//       if (scenario.murderer === murderer) {
+//         scenario.weapon = 'Candle Stick';
+//       }
+//     }
+//
+//     unexpectedOutcome('Colonel Mustard');
+//   }
+//
+//   plotTwist('Dining Room');
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is ${scenario.weapon}.`
+// }
+//
+// changeScenario();
+// const verdict = declareWeapon();
+// console.log(verdict);
+// ```
+// Episode 8 Answer:
+// Answer: Error message
+// Reason: Attempting to reassign a const
+//
+// #### Episode 9
+//
+// ```js
+// let murderer = 'Professor Plum';
+//
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+// ```
+//
+// ### Extensions
+//
+// Make up your own episode!
